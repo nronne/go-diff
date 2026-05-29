29b1cc921c7127369cc1238a4caf393869b63b3b
Nikolaj Rønne <niron@thul.fysik.dtu.dk>
2026-05-29 10:06:10 +0200

    fix: example_script.py

--- scripts/example_script.py
+++ scripts/example_script.py
@@ -4,11 +4,11 @@ from argparse import ArgumentParser
 from ase.build import fcc111, surface
 from mace.calculators import mace_mp
 
-from agedi import create_diffusion
+from agedi import AtomsGraph, create_diffusion
 
 from go_diff import GODiff
 from go_diff.controllers import SampleController, TemperatureSchedule, MomentumConsensusStop
-from go_diff.noisers import WeightedPositions
+from go_diff.noisers import WeightedConfinedCellPositions
 
 parser = ArgumentParser(description="Train a diffusion model for GO tasks.")
 parser.add_argument('-i', '--index', type=int, default=0, help="Index for the training run.")
@@ -16,15 +16,13 @@ args = parser.parse_args()
 index = args.index
 
 
-
 ##### HYPERPARAMETERS #####
 name = __file__.split('/')[-1].split('.')[0]  # use the filename as the name of the experiment
 
 initial_buffer_size = 16
 min_E = -200                    # to avoid false minimas in the MACE potential
-
 n_atoms = 1                     # number of atoms in the optimization
-atomic_numbers = [78] * n_atoms # atomic numbers of the atoms in the optimization (e.g., [78] for Pt)
+formula = "Pt"
 confinement_above_zmax = np.array([0.0, 4.0])  # confinement above the maximum z position of the template 
 
 
@@ -36,10 +34,13 @@ calc = mace_mp(model="medium", dispersion=False, default_dtype="float32", device
 template = surface('Pt', (1,2,2), 5, vacuum=8.0)
 template.positions[:, 2] -= template.positions[:, 2].min()
 
-confinement = list(confinement_above_zmax + template.positions[:, 2].max())
+confinement = confinement_above_zmax + template.positions[:, 2].max()
+template = AtomsGraph.from_atoms(template, confinement=confinement)
+
+
 
 #### DIFFUSION MODEL #####
-diffusion = create_diffusion(noisers=(WeightedPositions(),))
+diffusion = create_diffusion(noisers=(WeightedConfinedCellPositions(),))
 
 #### GO-DIFF #####
 
@@ -52,11 +53,15 @@ godiff = GODiff(
     training_controller=MomentumConsensusStop(min_steps=100, patience=250, drop_factor=0.9),
     sample_config={
         "template": template,
-        "atomic_numbers": atomic_numbers,
+        "formula": formula,
+        "confinement": confinement,
     },
     dataset_config={
         "mask": "MaskFixed",
         "confinement": confinement,
+    },
+    trainer_config={
+        "name": name
     },
     initial_buffer_size=initial_buffer_size,
     min_E=min_E,
