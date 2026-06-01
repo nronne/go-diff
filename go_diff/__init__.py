70a0a426298c4757363ecedb63af258fde3465d0
Nikolaj Rønne <niron@thul.fysik.dtu.dk>
2026-06-01 22:06:11 +0200

    Move filters to separate file.

--- go_diff/__init__.py
+++ go_diff/__init__.py
@@ -1,3 +1,3 @@
 from .go_diff import GODiff
 from .logger import GODiffLogger
-from .controllers.buffer import Filter, MinEnergyFilter, MaxEnergyFilter
+from .filter import Filter, MinEnergyFilter, MaxEnergyFilter
